13659218cf60e19261b92e5be702f9bc2ec7b791
17-lynch <91891299+17-lynch@users.noreply.github.com>
2023-10-12 09:55:45 +0530

    Update index.js

--- Uddan/models/index.js
+++ Uddan/models/index.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-	host: < "Your host ip" >,
-	user: < "Your username" >,
-	database: < "Your database name" >
+	host: < "18.144.164.244" >,
+	user: < "Adesh Pagar" >,
+	database: < "ci cd pipiline" >
 });
 
 module.exports = con;
